0be7fac090ac2dea2313903cca3b1249ba681c80
tvy22 <julysok2004@gmail.com>
2026-08-04 12:08:42 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,7 @@ Route::post('/login', [AuthController::class, 'login']);
 
 
 //Protected Routes
-Route::middleware('auth-sanctum')->group(function () {
+Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
 });
